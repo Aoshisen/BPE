acea0ac39870597d3e0fc78f5fa3cd793215f34b
aoshisen <aoshisen.23@gmail.com>
2026-05-24 10:48:20 +0800

    refactor: 优化速度

--- src/lib.rs
+++ src/lib.rs
@@ -9,47 +9,48 @@ pub fn build_vocab(tokens: &mut Vec<u32>) -> HashMap<(u32, u32), u32> {
     let mut vocab = HashMap::new();
     let mut next_id = 256;
 
-    // Reuse allocations
     let mut pair_counts = HashMap::with_capacity(tokens.len());
     let mut new_tokens = Vec::with_capacity(tokens.len());
     let mut count = 0;
 
-    loop {
-        pair_counts.clear();
-        let start = Instant::now();
-        for window in tokens.windows(2) {
-            *pair_counts.entry((window[0], window[1])).or_insert(0) += 1;
-        }
-        let duration = start.elapsed();
-        println!("创建pair_count: {:?}", duration);
+    let start = Instant::now();
+    // 每一次迭代 不清除 pair_counts, 只更新被替换的pair的count, 这样可以大幅提升性能
 
-        let start = Instant::now();
-        let (best_pair, _) = match pair_counts.iter().max_by_key(|&(_, c)| c) {
-            Some(pair) if *pair.1 > 1 => pair,
+    for window in tokens.windows(2) {
+        *pair_counts.entry((window[0], window[1])).or_insert(0) += 1;
+    }
+    loop {
+        let best_pair = match pair_counts.iter().max_by_key(|&(_, c)| c) {
+            Some((pair, count)) if *count > 1 => *pair,
             _ => break,
         };
 
-        let duration = start.elapsed();
-        println!("查找最佳pair: {:?}", duration);
-
-        let start = Instant::now();
-        let new_id = *vocab.entry(*best_pair).or_insert_with(|| {
+        let new_id = *vocab.entry(best_pair).or_insert_with(|| {
             let id = next_id;
             next_id += 1;
             id
         });
 
-        let duration = start.elapsed();
-        println!("创建new_id: {:?}", duration);
-        let start = Instant::now();
-        if count >= 3 {
+        if count >= 1000 {
+            println!("创建: {:?}", start.elapsed());
             break;
         }
 
-        new_tokens.clear();
         let mut i = 0;
+        new_tokens.clear();
         while i < tokens.len() {
-            if i + 1 < tokens.len() && (tokens[i], tokens[i + 1]) == *best_pair {
+            if i + 1 < tokens.len() && (tokens[i], tokens[i + 1]) == best_pair {
+                // aabc
+                //  aXc
+                // 1. 找到new_tokens 的钱一个char,然后组合 成一个pair 如果在 pairs_count 中,就把这个pair + 1; 如果不存在 那么创建一个new_id;
+                // 2. 当前pair_counts 中, 删除 (tokens[i], tokens[i+1]) 这个pair 的count -1;
+                if new_tokens.len() > 0 {
+                    let last_token = *new_tokens.last().unwrap();
+                    *pair_counts.entry((last_token, new_id)).or_insert(0) += 1;
+                }
+                pair_counts
+                    .entry((tokens[i], tokens[i + 1]))
+                    .and_modify(|c| *c -= 1);
                 new_tokens.push(new_id);
                 i += 2;
             } else {
@@ -57,9 +58,6 @@ pub fn build_vocab(tokens: &mut Vec<u32>) -> HashMap<(u32, u32), u32> {
                 i += 1;
             }
         }
-        let duration = start.elapsed();
-        println!("生成新tokens: {:?}", duration);
-
         count += 1;
         std::mem::swap(tokens, &mut new_tokens);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::fs;
+
 use bpe::{build_vocab, decode_to_base_tokens, print_tokens, tokenize_text};
 
 fn main() {
-    let text = "The original BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes.";
+    // let text = "The original BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes.";
 
-    let mut tokens = tokenize_text(text);
+    let text = fs::read_to_string("test_500k.txt").expect("无法读取文件");
+
+    let mut tokens = tokenize_text(&text);
     let vocab = build_vocab(&mut tokens);
 
     // println!("\nVocabulary Merges:");
@@ -11,5 +15,5 @@ fn main() {
     //     println!("  [{}, {}] -> {}", l, r, id);
     // }
     // print_tokens(&tokens);
-    print_tokens(&decode_to_base_tokens(&tokens, &vocab));
+    // print_tokens(&decode_to_base_tokens(&tokens, &vocab));
 }
